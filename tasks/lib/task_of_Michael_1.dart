d0838e2d41751885ece45b61850a452275e72f47
dmitriytsvetkov <dimon-37@list.ru>
2023-03-03 13:02:12 +0800

    first commit

---
+++ tasks/lib/task_of_Michael_1.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class TextWithDollar extends StatelessWidget {
+  final String title;
+  const TextWithDollar({super.key, required this.title});
+   @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextFormField(),
+    );
+  }
+}
---
+++ test/lib/future.dart
@@ -0,0 +1,11 @@
+//
+// void main () {
+//   print(1);
+//   print(2);
+//   print(3);
+//   Future waitingFuture = Future<Object?>.delayed(Duration(seconds: 10));
+//   waitingFuture.then((_) => print(4));
+//   print(5);
+//   print(6);
+//   print(7);
+// }
